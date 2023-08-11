914826c2967c359103b6852e891f78fd87e867b5
Nishanth22111 <141021010+Nishanth22111@users.noreply.github.com>
2023-08-11 11:43:38 +0530

    CS206FEWDLABEXP_1B

---
+++ proj.R
@@ -0,0 +1,56 @@
+v <- "chair"
+print(class(v))
+
+f = 3.14
+print(class(f))
+ 
+x <- 4
+print(class(x))
+
+d <- F
+print(class(d))
+
+
+a <- round (3.141592653589793, digit = 4)
+print (digit)
+print (a)
+
+b = round (3.141592653589793, digits <- 2)
+print (digits)
+print (b)
+
+c <- round (3.141592653589793, digit = -3)
+print (digit)
+print (c)
+
+r <- 22L
+print (class(r))
+
+s <- 2+5i
+print (class(s))
+
+t <- charToRaw("Hello")
+print (class(t))
+
+y <- c(4,1,6,3)
+print (y)
+
+apple <- c("red","green",'yellow')
+print (apple)
+print(class(apple))
+
+p <- c(T,T,F,T,F)
+print(class(p))
+print(p)
+
+
+u <- c(2,3,1,4)
+print (u[-1])
+u[3]
+u[c(1,3,4)]
+length(u)
+u[c(2:4)]
+v <- c(2L,TRUE,2.3123)
+print (class(v))
+
+
